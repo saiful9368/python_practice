4130a9b88d901347f9b230eeda19bb87e416c996
SaifulRahman <sr8279717071@gmail.com>
2022-10-01 15:54:04 +0530

    addition

---
+++ add.py
@@ -0,0 +1,7 @@
+#create two variables and assign values to them
+#create a third variable (sum) and x and y
+
+x=int(input("enter the value of x = "))
+y=int(input("enter the value of y = "))
+sum = x+y
+print("the sum of x and y is -:",sum)
