c4fa500a7be41f896b10d4b2bcd59ff90e007b44
SaifulRahman <sr8279717071@gmail.com>
2022-10-16 13:35:30 +0530

    random function

---
+++ RanddoM.py
@@ -0,0 +1,11 @@
+import random
+
+x=random.randint(1,19)
+print(x)
+y=random.randrange(1,20)
+print(y)
+z=["dog","cat","goat","wolf","ox","bull"]
+
+for i in range(5):
+    i=random.choice(5)
+    print("akshat",z[i])
