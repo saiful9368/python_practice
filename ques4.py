d55099163ea004eccc4828a62193324979af959e
SaifulRahman <sr8279717071@gmail.com>
2022-11-12 09:24:15 +0530

    Question3

---
+++ ques4.py
@@ -0,0 +1,13 @@
+list1=["Phil","Oz","Seuss","Dre"]
+list2=[]
+for i in list1:
+    x="Dr."+i
+    list2.append(x)
+    #print(x)
+print(list2)
+
+
+
+"""    j=list(i)+["Dr.prefix"]
+    list2=list2+j
+print(list2)"""
