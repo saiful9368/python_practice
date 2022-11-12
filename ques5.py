8460c3536ebd5a7a567d2026b6a703ff82398874
SaifulRahman <sr8279717071@gmail.com>
2022-11-12 09:24:55 +0530

    Question5

---
+++ ques5.py
@@ -0,0 +1,10 @@
+num=int(input())
+factorial=1
+if num<0:
+    print("input must be greater than zero")
+elif num==0:
+    print("the factorial of 0 is 1")
+else:
+    for i in range(1,num+1):
+        factorial=factorial*i
+    print("the factorial of",num,"is",factorial)
