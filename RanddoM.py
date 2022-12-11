bf4ec66180c47078051018eb8eaa90bbc6cc00d8
SaifulRahman <sr8279717071@gmail.com>
2022-12-11 13:05:27 +0530

    random:

--- RanddoM.py
+++ RanddoM.py
@@ -4,8 +4,3 @@ x=random.randint(1,19)
 print(x)
 y=random.randrange(1,20)
 print(y)
-z=["dog","cat","goat","wolf","ox","bull"]
-
-for i in range(5):
-    i=random.choice(5)
-    print("akshat",z[i])
