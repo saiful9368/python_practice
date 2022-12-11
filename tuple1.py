2bcf2b12c09bc48e1e673634dfce37d511f95e7a
SaifulRahman <sr8279717071@gmail.com>
2022-12-11 13:03:31 +0530

    tuple ques

---
+++ tuple1.py
@@ -0,0 +1,32 @@
+#packing and unpacking
+'''tuple1=(1,2,3,4,5)
+(one,two,three,four,five)=tuple1
+print(one)'''
+
+'''tuple2=("car","bike","boat","cycle")
+(item1,*item2,item3)=tuple2
+print(item1)
+print(item2)
+print(item3)
+
+tuple3=(10,20,30,40)
+(a,b,c,d)=tuple3
+print(a,b,c,d)
+
+tuple1 = ("apple", "banana", "cherry")
+i = 0
+while i < len(tuple1):
+  print(tuple1[i])
+  i = i + 1
+
+tuple1=(10,20,30,40,20)
+tuple2=(tuple1[1],tuple1[2])
+print(tuple2)
+print(tuple1.count(20))'''
+
+x=input()
+y=input()
+z=input()
+a=input()
+tup=(x,y,z,a)
+print(tup)
