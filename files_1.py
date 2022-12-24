901ef12ba71d6410df4f9b9ebd19f0f89bdd8cfb
SaifulRahman <sr8279717071@gmail.com>
2022-12-24 10:16:34 +0530

    creating files

---
+++ files_1.py
@@ -0,0 +1,14 @@
+# f=open("demo.txt","r")
+# #print(f.read())
+# print(f.readline())
+# print(f.readline())
+
+# f=open("demo.txt","a",encoding="utf-8")
+# print(f.read())
+
+f2=open("demo2.txt","w")
+f2.write("this is a new file creating by coding")
+
+
+f3=open("myself.txt","w+")
+f3.write("My name is Saiful Rahman \nI am btech CSE student at\nLovely proffesional University")
